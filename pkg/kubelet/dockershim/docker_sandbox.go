a337048cf96806877e72d9da399291843ca41dbd
S E Livingstone <livingstone.se@flipkart.com>
2020-05-02 17:17:02 +0530

    Removed checking the networkReady condition before calling cni del in the stop podsandbox flow of dockershim. This is done because there were cases where the cni del was being skipped in the stop podsandbox flow when the networkReady flag for the pod sandbox was false. The networkReady flag for the pod sandbox is false when the cni plugin is executing. This check can be removed because as per the cni spec, it is legal to call the cni add multiple times, while as per contract cni del must be called at least once after cni add is called.

--- pkg/kubelet/dockershim/docker_sandbox.go
+++ pkg/kubelet/dockershim/docker_sandbox.go
@@ -250,8 +250,7 @@ func (ds *dockerService) StopPodSandbox(ctx context.Context, r *runtimeapi.StopP
 	// since it is stopped. With empty network namespcae, CNI bridge plugin will conduct best
 	// effort clean up and will not return error.
 	errList := []error{}
-	ready, ok := ds.getNetworkReady(podSandboxID)
-	if !hostNetwork && (ready || !ok) {
+	if !hostNetwork {
 		// Only tear down the pod network if we haven't done so already
 		cID := kubecontainer.BuildContainerID(runtimeName, podSandboxID)
 		err := ds.network.TearDownPod(namespace, name, cID)
